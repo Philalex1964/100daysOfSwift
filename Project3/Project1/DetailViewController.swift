a8881fedf4831a6d40e7e7373eaf2449a381e9a1
Alexander <philalex196401@gmail.com>
2021-02-20 09:56:29 +0100

    Project 3 ready + Challenge 1.

---
+++ Project3/Project1/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Alexander Filippov on 10.2.21..
+//  Copyright © 2021 Alexander Filippov. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped () {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found.")
+            return
+        }
+
+//        Challenge 1
+//        guard let imageName = selectedImage else { return }
+//        let vc = UIActivityViewController(activityItems: [imageName], applicationActivities: [])
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
