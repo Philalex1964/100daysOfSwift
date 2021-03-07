89c72e1c927527a7dee9c1b1a0b7da9c0c0e2143
Philalex <philalex196401@gmail.com>
2021-03-07 17:01:24 +0100

    Day 35. Challenges 1-3.

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Petitions"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filter))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
         
         let urlString: String
         
@@ -27,6 +34,7 @@ class ViewController: UITableViewController {
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                filteredPetitions = petitions
                 return
             }
         }
@@ -38,7 +46,7 @@ class ViewController: UITableViewController {
         let decoder = JSONDecoder()
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            petitions = jsonPetitions.results
+            petitions = jsonPetitions.results            
             tableView.reloadData()
         }
     }
@@ -49,13 +57,60 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "The data comes from the We The People API of the Whitehouse.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func filter() {
+        let ac = UIAlertController(title: "Filter", message: "Enter search", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let filterPetitions = UIAlertAction(title: "Ok", style: .default) { [weak self, weak ac] _ in
+            guard let search = ac?.textFields?[0].text else { return }
+            self?.submit(search)
+        }
+        
+        let clearSearch = UIAlertAction(title: "Clear search", style: .default) { [weak self] _ in
+            self?.clear()
+        }
+        
+        ac.addAction(filterPetitions)
+        ac.addAction(clearSearch)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ search: String) {
+        let lowerSearch = search.lowercased()
+        
+        if !lowerSearch.isEmpty {
+            filteredPetitions.removeAll(keepingCapacity: true)
+            tableView.reloadData()
+            
+            for petition in petitions {
+                if petition.title.lowercased().contains(lowerSearch) || petition.body.lowercased().contains(lowerSearch) {
+                    filteredPetitions.insert(petition, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
+    
+    func clear() {
+        filteredPetitions = petitions
+        tableView.reloadData()
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let  petition = petitions[indexPath.row]
+        let  petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         
@@ -64,7 +119,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 } 
