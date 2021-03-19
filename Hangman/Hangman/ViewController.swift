17c6f8e04df67d00e278f08845c6ceb73769a9b2
Philalex <philalex196401@gmail.com>
2021-03-19 17:31:50 +0100

    Return to fulfill a challenge!

---
+++ Hangman/Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Alexander Filippov on 19.3.21..
+//  Copyright © 2021 Alexander Filippov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
