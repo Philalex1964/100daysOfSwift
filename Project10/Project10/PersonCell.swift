c683e86a213c654b433bfa42f469b1616b73b40b
Philalex <philalex196401@gmail.com>
2021-03-20 11:11:05 +0100

    Day 42. Project 10. Part 1. UICollectionView and UICollectionViewCell.

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Alexander Filippov on 20.3.21..
+//  Copyright © 2021 Alexander Filippov. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Alexander Filippov on 19.3.21..
+//  Copyright © 2021 Alexander Filippov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        return cell
+    }
+
+}
+
